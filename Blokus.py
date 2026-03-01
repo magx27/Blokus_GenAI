9ec3ac0a70295193868f6bd5f470673fffeaa3bd
Nicolas Alejandro Zevallos Chavez <nicolas.zevallos.chavez.2000@gmail.com>
2026-03-01 17:32:34 +0100

    Logic, and UI for the Blokus game.

--- Blokus.py
+++ Blokus.py
@@ -21,15 +21,15 @@ class Game:
         self.board = Board(self)   
         self.players = [Player("red"), Player("blue"), Player("green"), Player("yellow")]
         self.current_player = self.players[0]
-        self.selected_piece = "three_L"   # for now we just select one piece, but we can later implement a UI to select different pieces just like the game
+        self.selected_piece = "one"   # for now we just select one piece, but we can later implement a UI to select different pieces just like the game
 
     def select_piece(self, piece_name):
         self.selected_piece = piece_name
 
     def on_click(self, row, col):
         if self.selected_piece:
-            self.board.place_piece(self.selected_piece, row, col, self.current_player.color)
-            self.switch_player()
+            if self.board.place_piece(self.selected_piece, row, col, self.current_player.color, self.current_player):
+                self.switch_player()
 
     def start(self):
         self.root.mainloop()
@@ -62,8 +62,24 @@ class Board:
             buttons.append(row_buttons)
         return buttons
 
-    def place_piece(self, selected_piece, row, col, color):
+    def place_piece(self, selected_piece, row, col, color, player):
         piece = pieces[selected_piece]
+
+        # 0. FIRST MOVE RULE: must cover ANY corner
+        if not player.has_played:
+            corners = [(0, 0), (0, 19), (19, 0), (19, 19)]
+            covers_corner = False
+
+            for dr, dc in piece:
+                r = row + dr
+                c = col + dc
+                if (r, c) in corners:
+                    covers_corner = True
+                    break
+
+            if not covers_corner:
+                print("First move must cover ANY corner of the board!")
+                return False
 
         # 1. Check bounds
         for dr, dc in piece:
@@ -81,11 +97,41 @@ class Board:
                 print("Cell already occupied!")
                 return False
 
-        # 3. Place the piece
+        # 3. Blokus rule: only apply AFTER first move
+        if player.has_played:
+            touches_corner = False
+
+            for dr, dc in piece:
+                r = row + dr
+                c = col + dc
+
+                # Edge neighbors (forbidden)
+                edge_neighbors = [(r-1, c), (r+1, c), (r, c-1), (r, c+1)]
+                for nr, nc in edge_neighbors:
+                    if 0 <= nr < 20 and 0 <= nc < 20:
+                        if self.buttons[nr][nc]["bg"] == color:
+                            print("Cannot touch your own piece on an edge!")
+                            return False
+
+                # Corner neighbors (required)
+                corner_neighbors = [(r-1, c-1), (r-1, c+1), (r+1, c-1), (r+1, c+1)]
+                for nr, nc in corner_neighbors:
+                    if 0 <= nr < 20 and 0 <= nc < 20:
+                        if self.buttons[nr][nc]["bg"] == color:
+                            touches_corner = True
+
+            if not touches_corner:
+                print("Must touch your own piece at a corner!")
+                return False
+
+        # 4. Place the piece
         for dr, dc in piece:
             r = row + dr
             c = col + dc
             self.buttons[r][c].config(bg=color)
+
+        # Mark first move as done
+        player.has_played = True
         return True
 
 
@@ -93,6 +139,7 @@ class Player:
     def __init__(self, color):
         self.color = color
         self.pieces = ["one", "two_horizontal", "three_L"]
+        self.has_played = False
 
     def select_piece(self):
         # For now we just return the same piece, but we can later implement a UI to select different pieces just like the game
