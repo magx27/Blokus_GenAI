3384f7c3468b836511f5c817f679dcf6f78f578c
Nicolas Alejandro Zevallos Chavez <nicolas.zevallos.chavez.2000@gmail.com>
2026-03-01 16:07:44 +0100

    version 2.0

--- Blokus.py
+++ Blokus.py
@@ -4,13 +4,22 @@ from functools import partial
 # We can use this to later handle the button to put the pieces
 # For now, it just prints the coordinates of the button clicked
 def on_click(row, col, buttons):
-    place_piece("example_piece", row, col, buttons)
+    selected_piece = select_piece()
+    place_piece(selected_piece, row, col, buttons)
 
 ### we have to later create the logic to check if the piece can be placed in the selected position
-def place_piece(piece, row, col, buttons):
-    # Access the button at the clicked position
-    btn = buttons[row][col]
-    btn.config(bg="red")  # Change the color of the button
+def place_piece(selected_piece, row, col, buttons):
+    piece = selected_piece
+
+    for dr, dc in piece:
+        r = row + dr
+        c = col + dc
+
+        # Check board boundaries
+        if 0 <= r < 20 and 0 <= c < 20:
+            buttons[r][c].config(bg="red")
+        else:
+            print("Piece goes out of bounds!")
 
 def create_board():
     root = tk.Tk()
@@ -35,8 +44,19 @@ def create_board():
     return buttons
 
 ### we have to later create the pieces and the logic to select them.
-def select_piece(piece):
-    print(f"Selected piece: {piece}")
+def select_piece():
+    # For now we just return the same piece, but we can later implement a UI to select different pieces just like the game
+    return pieces["three_L"]
 
+#### Here we define the pieces of the game, we can later add more pieces and also the logic to rotate and flip them.
+pieces = {
+    "one": [(0, 0)],
+
+    "two_horizontal": [(0, 0), (0, 1)],
+    "two_vertical":   [(0, 0), (1, 0)],
+
+    "three_line":     [(0, 0), (0, 1), (0, 2)],
+    "three_L":        [(0, 0), (1, 0), (1, 1)]
+}
 #### Here we call the functions for the game
 board = create_board()
